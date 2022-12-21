382a86fc25c1aec02c49b682518fc8902d4d7183
Alejandro Herrera Merlo <alex13pks@gmail.com>
2022-12-21 15:49:09 -0600

    Added some responsive design

--- script.js
+++ script.js
@@ -2,26 +2,25 @@
 // true = x
 // false = o
 let turno = true;
-let gameBoard = 
-[['','',''],
- ['','',''],
- ['','','']]
+let gameBoard = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+];
 const board = document.getElementById('board').querySelectorAll('div');
 console.log(board);
 
-function checkWin() {
-
-}
+function checkWin() {}
 
 board.forEach((cell, index) => {
   cell.addEventListener('click', (event) => {
     turno = !turno;
-    let row = index;
-    let cellIndex;
-    console.log(index, row)
-    if(event.target.textContent !== '') {return}
+    let row = Math.floor(index / 3);
+    let col = index % 3;
+    console.log(`Row: ${row}, Column: ${col}`);
+    if (event.target.textContent !== '') {
+      return;
+    }
     event.target.textContent = turno ? 'x' : 'o';
   });
-
-})
-
+});
